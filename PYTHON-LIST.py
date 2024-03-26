f19e2e7735b400ca28ec13eac1e1f6e1c00ed10b
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-03-26 10:56:46 -0500

    Add files via upload

---
+++ PYTHON-LIST.py
@@ -0,0 +1,28 @@
+ages  = [10,34,56]
+print(ages)
+
+student =['jack',32,'computer scince ']
+print(student)
+
+empty_lsit=[]
+print(empty_lsit)
+
+x ='a,y,z'
+result = list(x)
+print(result)
+
+fruits =['apple','orange','banana']
+print(fruits)
+fruits.append('cherry')
+print(fruits)
+
+fruits.insert(2,'mango')
+print(fruits)
+
+fruits.remove('orange')
+print(fruits)
+
+print(len(fruits))
+
+for fruits  in  fruits:
+print(fruits)
